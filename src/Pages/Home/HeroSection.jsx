a6b0500f3d9605de8a83e812f45d24a62cc6e6c4
Pernillemort <pernillemort1998@gmail.com>
2025-06-16 09:59:39 +0200

    Fix image path

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
                 </div>
             </div>
             <div className="hero--section--img">
-                <img src="./img/hero.png" alt="Hero Section" />
+                <img src="./img/Hero.png" alt="Hero Section" />
             </div>
         </section>
     );
